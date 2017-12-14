75f54160bf77f83a2f2ad1cbca0da4233f5000b1
Victor Ribero <victor.ribero3@gmail.com>
2017-12-14 22:42:35 +0100

    [NEW] First commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/entry.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+    ]
+  }
+};
